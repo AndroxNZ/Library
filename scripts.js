adb2cc49f13ad1db5353ef0888381fe1a8d6e8a4
Andrew Parker <andrewtparker@gmail.com>
2024-02-10 17:32:50 +1300

    everything but toggle working

--- scripts.js
+++ scripts.js
@@ -1,39 +1,44 @@
 const myLibrary = [];
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295);
-const LOTRs = new Book("The Lord of the Rings", "J.R.R Tolkien", 1178);
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, true);
+const LOTRs = new Book("The Lord of the Rings", "J.R.R Tolkien", 1178, true);
 myLibrary.push(theHobbit, LOTRs);
 console.log(myLibrary[0]);
 let title, author, pages, read;
-const showDialog = document.querySelector(".showDialog");
-const enterBookDialog = document.querySelector("#enter-book");
+
 const deleteButton = document.querySelector(".close");
 const submitButton = document.querySelector("button[type='submit'");
 const library = document.querySelector(".library");
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = null;
+  this.read = false;
   // this.info = function () {
   //   console.log(`${this.title} by ${this.author}, ${pages} pages, ${read}`);
   // };
 }
-// Book.prototype.hasRead() {
-//     read === true ? false : true;
-// };
+Object.defineProperty(Book.prototype, "hasRead", {
+  enumerable: false,
+  value: function () {
+    this.read === true ? false : true;
+  },
+});
 function addBookToLibrary() {
   title = document.querySelector('input[name="title"]').value;
   author = document.querySelector('input[name="author"]').value;
   pages = parseInt(document.querySelector('input[name="pages"]').value);
-  let book = new Book(title, author, pages, read);
+  let book = new Book(title, author, pages);
   myLibrary.push(book);
-  addBook(book);
+  displayBooks(book);
   console.log(book);
   console.log(myLibrary);
 }
 function displayBooks() {
   let tableBody = document.querySelector("tbody");
+  while (tableBody.firstChild) {
+    tableBody.removeChild(tableBody.firstChild);
+  }
   for (let entry of myLibrary) {
     let tableRow = document.createElement("tr");
     for (let prop in entry) {
@@ -42,24 +47,36 @@ function displayBooks() {
       // console.log(`curent prop is ${entry.prop}`);
       tableRow.appendChild(currentElement);
     }
+    let delButton = document.createElement("button");
+    delButton.innerText = "Delete";
+    tableRow.appendChild(delButton);
+    let readStatus = document.createElement("button");
+    readStatus.innerText = "Toggle Read";
+    tableRow.appendChild(readStatus);
     tableBody.appendChild(tableRow);
   }
+  let delButtons = library.querySelectorAll("button");
+  delButtons.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      event.target.closest("tr").remove();
+    });
+  });
 }
-
 displayBooks();
-function addBook(book) {
-  let tableBody = document.querySelector("tbody");
-  let tableRow = document.createElement("tr");
-  for (let prop in book) {
-    let currentElement = document.createElement("td");
-    currentElement.innerText = book[prop];
-    // console.log(`curent prop is ${entry.prop}`);
-    tableRow.appendChild(currentElement);
-  }
-  tableBody.appendChild(tableRow);
-}
+
+// function addBook(book) {
+//   let tableBody = document.querySelector("tbody");
+//   let tableRow = document.createElement("tr");
+//   for (let prop in book) {
+//     let currentElement = document.createElement("td");
+//     currentElement.innerText = book[prop];
+//     // console.log(`curent prop is ${entry.prop}`);
+//     tableRow.appendChild(currentElement);
+//   }
+//   tableBody.appendChild(tableRow);
+// }
 
 submitButton.addEventListener("click", (event) => {
-  event.preventDefault();
   addBookToLibrary();
+  event.preventDefault();
 });
